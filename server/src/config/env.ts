e88c2bdd56ca01faba8719fd47a363d87bd78fe1
Shadan Rashid <shadanrashid786@gmail.com>
2026-02-28 12:24:05 +0000

    remove the type error in config.env

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -2,6 +2,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined in environment variables");
+}
+
 export const config = {
     port: process.env.PORT ,
     nodeEnv: process.env.NODE_ENV ,
